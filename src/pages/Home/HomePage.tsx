28370bc398509b86c23d7ed506d47c30cdaa67a2
SarahSu92 <sarah.sundberg@outlook.com>
2026-01-07 17:47:13 +0100

    (feat): added content to home page

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,22 +1,65 @@
 import { Link } from 'react-router';
 import './Home.scss';
+import {
+  DestinationCards,
+  type DestinationCard,
+} from '../../models/DestinationCard';
 
 export const HomePage = () => {
-    return (
-        <>
-         <div className="herosection">
-        <img
-          className="heroimage"
-          src={`${import.meta.env.BASE_URL}zoo-animals.jpg`}
-          alt=""
-        />
-        <h1 className="heroh1">Find your next travel adventure</h1>
+  return (
+    <>
+      <section className="hero">
+         <div className="hero-overlay">
+        <div className="hero-content">
+          <h1>Adventure Awaits!</h1>
+          <p>
+            Discover new destinations, meet new cultures, and have fun along the way.
+          </p>
+          <Link to="/DestinationPage" className="herobtn">
+            EXPLORE NOW
+          </Link>
+        </div>
+        <div className="hero-wave">
+          <svg viewBox="0 0 1440 320" preserveAspectRatio="none">
+            <path
+              d="M0,80 C360,250 1080,-50 1440,120 L1440,320 L0,320 Z"
+              fill="#fff"
+            />
+          </svg>
+        </div>
+        </div>
+      </section>
+
+      <div className="herocards">
+        <div className="dest-cards">
+          {DestinationCards.slice(0, 3).map((dest: DestinationCard) => (
+            <div key={dest.id} className="destination-card">
+              <img
+                className="img"
+                src={`${import.meta.env.BASE_URL}${dest.image}`}
+                alt={dest.name}
+              />
+              <Link to={`/destination/${dest.id}`}>
+                <h3>{dest.name}</h3>
+                <div className="activities">
+                  {dest.activities.map((activity) => (
+                    <li key={activity} className="activity">
+                      {activity}
+                    </li>
+                  ))}
+                </div>
+                <p>{dest.shortDescription}</p>
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
       <div className="heroinfo">
-        <Link to={`/DestinationPage`} className="herolink">
-        <h2 className='herolink'>See all our destinations!</h2>
+        <h2>About </h2>
+        <Link to={`/DestinationPage`} className="">
+          See all our destinations!
         </Link>
       </div>
-        </>
-    );
+    </>
+  );
 };
